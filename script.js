4e3710df839ed88a5c43fb035ed657acc2f01264
Rebecca Otero <iotero07@gmail.com>
2024-11-06 11:07:02 -0500

    background color practice

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let backgroundColor = "white";
+
+let fontColor = "blue";
+
+backgroundColor = prompt("What color do you want to change the background to?");
+
+console.log("You have chosen the color" + backgroundColor);
+
+document.body.style.background = backgroundColor;
+
+document.body.styleColor = fontColor 
